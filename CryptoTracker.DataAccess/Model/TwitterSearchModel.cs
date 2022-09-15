6c9ebdb8d903d04e2fd0558a5f454938c682d093
cbonell <coltonbonell@gmail.com>
2022-09-15 19:33:36 -0400

    Add twitter access via api

---
+++ CryptoTracker.DataAccess/Model/TwitterSearchModel.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace CryptoTracker.DataAccess.Model;
+
+public class TwitterSearchModel
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+    [JsonProperty("text")]
+    public string Text { get; set; } = "";
+}
