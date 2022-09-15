c6e6588e212d7f5c7f365f550ecb9ee6d1c9226b
cbonell <coltonbonell@gmail.com>
2022-09-15 19:32:52 -0400

    Add coin gecko access

---
+++ CryptoTracker.DataAccess/CoinGeckoAccess/CoinGeckoData.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace CryptoTracker.DataAccess.CoinGeckoAccess;
+
+public class CoinGeckoData
+{
+    public static async Task<double> GetPriceInUsd(string currency)
+    {
+        // 
+        RestClient client = new RestClient($"https://api.coingecko.com/api/v3/simple/price?ids={currency}&vs_currencies=usd");
+        RestRequest request = new RestRequest()
+        {
+            Method = Method.Get
+        };
+        RestResponse response = await client.ExecuteAsync(request);
+        JObject data = JObject.Parse(response.Content!);
+        if (data != null)
+        {
+            JToken token = data.First;
+            double m = JsonConvert.DeserializeObject<double>(token.First.First.ToString())!;
+            return m;
+        }
+        return 0;
+    }
+}
