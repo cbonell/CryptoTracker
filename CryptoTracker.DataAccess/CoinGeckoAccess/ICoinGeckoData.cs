ce62db869c1b52a09c5505b714d5be8e6605f099
cbonell <72631543+cbonell@users.noreply.github.com>
2022-09-26 22:40:14 -0400

    Add live price data for 24hr price chart and fix rendering of certain coin details

--- CryptoTracker.DataAccess/CoinGeckoAccess/ICoinGeckoData.cs
+++ CryptoTracker.DataAccess/CoinGeckoAccess/ICoinGeckoData.cs
@@ -2,6 +2,7 @@
 {
     public interface ICoinGeckoData
     {
+        Task<IEnumerable<Tuple<double, string>>> GetPriceHistory(string currency);
         Task<double> GetPriceInUsd(string currency);
     }
 }
