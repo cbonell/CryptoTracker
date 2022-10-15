9132949742b7dbcc67cd26a99e66df9fbade0f85
cbonell <72631543+cbonell@users.noreply.github.com>
2022-10-15 01:22:04 -0400

    Add TwitterSearch unit tests

---
+++ CryptoTracker.Tests/Twitter/TwitterSearchTests.cs
@@ -0,0 +1,30 @@
+﻿using CryptoTracker.DataAccess.Twitter;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTracker.Tests.Twitter;
+
+[TestClass]
+public class TwitterSearchTests
+{
+    [TestMethod]
+    public async Task TwitterSearch_InvaidCoinName_ExpectException()
+    {
+        // Arrange
+        Exception? exception = null;
+
+        // Act
+        try
+        {
+            await TwitterSearch.ByCoin(null);
+        }
+        catch(ArgumentNullException e) { exception = e; }
+
+        // Assert
+        Assert.IsNotNull(exception);
+    }
+}
