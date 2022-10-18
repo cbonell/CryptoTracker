b9c30ba9192dfda4cd95da4026ff1fd4fe992970
cbonell <72631543+cbonell@users.noreply.github.com>
2022-10-18 19:35:11 -0400

    Add true trending tweets for coins (#45)
    * Add true trending tweets for coins
    
    * Add link to tweeter

--- SharedConstants/Constants.cs
+++ SharedConstants/Constants.cs
@@ -5,13 +5,14 @@ public class Constants
     // Test user data
     public static readonly string TestUser_UserId = "testuser:systemtestuser";
 
-    ////// Data Base Constants //////
+    // Data Base Constants
     public const int dbval_CurrencyType_Coin = 101;
     public const int dbval_CurrencyType_Fiat = 100;
 
-    ///// Twitter API
+    // Twitter API
     public const string ApiKey = "OB0OuKLC4LkdAHriit93nAqUW";
     public const string ApiKeySecret = "kLI7DkRPEmPfLHEFqTKFhrOrTaHfPWfUxm3Xdt2g94Iwqurdpk";
+    public const string TwitterApiBearerToken = "AAAAAAAAAAAAAAAAAAAAAGmAhAEAAAAAO%2BbYhX%2FmtqyYtjdyG8uLXWgWFxs%3DJTjORZ8Xit9UdPE9HRfp7ZqlWgBTCfbKdg4075AmlLe88DWube";
     public const string BearerToken = "AAAAAAAAAAAAAAAAAAAAAGmAhAEAAAAAFV190k%2FVMnf%2BWvVz3IOKen1Buso%3DhsB39Ka1AY2TmQtBvs0S9AtMYB9kAFCe8cgVGvFp8uivXD0WRl";
 
 
