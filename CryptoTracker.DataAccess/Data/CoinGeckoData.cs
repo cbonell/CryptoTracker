77fd5a4fcc1a87b1c902f35f2d34db145a8c3e65
cbonell <72631543+cbonell@users.noreply.github.com>
2022-10-25 20:30:45 -0400

    Add ohlc retrieval

--- CryptoTracker.DataAccess/Data/CoinGeckoData.cs
+++ CryptoTracker.DataAccess/Data/CoinGeckoData.cs
@@ -19,7 +19,7 @@ public class CoinGeckoData : DataBase, ICoinGeckoData
 
     public async Task<double> GetPriceInUsd(string currency)
     {
-        if(string.IsNullOrWhiteSpace(currency))
+        if (string.IsNullOrWhiteSpace(currency))
         {
             throw new ArgumentNullException(nameof(currency));
         }
@@ -39,7 +39,7 @@ public class CoinGeckoData : DataBase, ICoinGeckoData
             if (data != null)
             {
                 JToken token = data.First;
-               price = JsonConvert.DeserializeObject<double>(token.First.First.First.ToString())!;
+                price = JsonConvert.DeserializeObject<double>(token.First.First.First.ToString())!;
             }
 
             _memoryCache.Set(cacheKey, price);
@@ -82,7 +82,7 @@ public class CoinGeckoData : DataBase, ICoinGeckoData
 
     public async Task<List<string>> GetTrending()
     {
-        List<string> coins = new ();
+        List<string> coins = new();
         if (!_memoryCache.TryGetValue(CacheKey.CoinGeckoGetTrending, out coins))
         {
             coins = new();
@@ -105,5 +105,69 @@ public class CoinGeckoData : DataBase, ICoinGeckoData
             _memoryCache.Set(CacheKey.CoinGeckoGetTrending, coins, cacheEntryOptions);
         }
         return coins;
+    }
+
+    /// <summary>
+    /// Returns OHLC pairs in 30 minute intervals
+    /// </summary>
+    /// <param name="coinName">Name of the coin</param>
+    /// <param name="days">Number of days to be returned (deffault of 1)</param>
+    /// <returns>List<OHLCPairModel></returns>
+    public async Task<List<OHLCPairModel>> GetOHLCPairs(string coinName, int days = 1)
+    {
+        //
+        if (string.IsNullOrEmpty(coinName) || days < 1)
+        {
+            throw new ArgumentException();
+        }
+
+        string cacheKey = $"GetOHLCPairs-{coinName}-{days}";
+        List<OHLCPairModel> responseData = new();
+        if (!_memoryCache.TryGetValue(cacheKey, out responseData))
+        {
+            responseData = new();
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetAbsoluteExpiration(TimeSpan.FromSeconds(30));
+
+            RestClient client = new RestClient();
+            RestRequest request = new RestRequest($"https://api.coingecko.com/api/v3/coins/{coinName}/ohlc?vs_currency=usd&days={days}");
+            RestResponse response = await client.ExecuteAsync(request);
+            JArray jArray = JArray.Parse(response.Content!);
+
+            if (jArray != null && jArray.Count > 0)
+            {
+                for (int i = 0; i < jArray.Count; i++)
+                {
+                    if (jArray[i] != null)
+                    {
+                        string? time = jArray[i][0]?.ToString();
+                        string? open = jArray[i][1]?.ToString();
+                        string? high = jArray[i][2]?.ToString();
+                        string? low = jArray[i][3]?.ToString();
+                        string? close = jArray[i][4]?.ToString();
+
+                        responseData.Add(new OHLCPairModel()
+                        {
+                            TimeStamp = DateTimeFromUnixTimestampMillis(long.Parse(time)),
+                            Open = double.Parse(open),
+                            High = double.Parse(high),
+                            Low = double.Parse(low),
+                            Close = double.Parse(close),
+                        });
+                    }
+                }
+            }
+
+
+            _memoryCache.Set(CacheKey.CoinGeckoGetTrending, responseData, cacheEntryOptions);
+        }
+
+        return responseData;
+    }
+
+    public static DateTime DateTimeFromUnixTimestampMillis(long millis)
+    {
+        DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        return UnixEpoch.AddMilliseconds(millis);
     }
 }
