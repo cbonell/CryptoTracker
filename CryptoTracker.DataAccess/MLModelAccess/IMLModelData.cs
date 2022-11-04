f9d49f039e97bd60f69f137aa47326508aaa3445
gerardocastro3 <73922245+gcastrolara@users.noreply.github.com>
2022-11-03 23:20:48 -0400

    Refactor MLModelData

--- CryptoTracker.DataAccess/MLModelAccess/IMLModelData.cs
+++ CryptoTracker.DataAccess/MLModelAccess/IMLModelData.cs
@@ -1,7 +1,13 @@
+using Newtonsoft.Json.Linq;
+
 namespace CryptoTracker.DataAccess.MLModelAccess
 {
     public interface IMLModelData
     {
         Task<List<DatePricePairModel>> GetPricePrediction(string coinSymbol);
+        List<OHLCPairModel> MinMaxNormalize(List<OHLCPairModel> features, string coinSymbol, Dictionary<string, double> values);
+        List<List<List<double>>> Tensorize(List<OHLCPairModel> features);
+        List<DatePricePairModel> convertToDPPM(JObject data, List<DatePricePairModel> predictions);
+        Task<JObject> MakeRequest(List<List<List<double>>> body, int model);
     }
 }
