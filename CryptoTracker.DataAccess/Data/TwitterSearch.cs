4ea947a2c484c0b106c88383eaf697230efa0173
cbonell <coltonbonell@gmail.com>
2022-11-09 17:35:04 -0500

    add blacklist word for tweet search

--- CryptoTracker.DataAccess/Data/TwitterSearch.cs
+++ CryptoTracker.DataAccess/Data/TwitterSearch.cs
@@ -7,6 +7,7 @@ namespace CryptoTracker.DataAccess.Data;
 
 public class TwitterSearch
 {
+    private static string[] blackListWords = { "giveaway", "winner" };
     public static async Task<IEnumerable<TweetSearchModel>> GetTrendingByHashTag(string hashTag)
     {
         if (string.IsNullOrEmpty(hashTag))
@@ -14,7 +15,7 @@ public class TwitterSearch
             throw new ArgumentNullException(nameof(hashTag));
         }
 
-        RestClient client = new RestClient($"  https://api.twitter.com/1.1/search/tweets.json?result_type=popular&lang=en&q=%23{hashTag}");
+        RestClient client = new RestClient($"https://api.twitter.com/1.1/search/tweets.json?result_type=popular&count=100&lang=en&q=(%23{hashTag} -{String.Join(" -", blackListWords)})");
         RestRequest request = new RestRequest()
         {
             Method = Method.Get
